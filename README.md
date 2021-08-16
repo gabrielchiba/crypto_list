39ecd6b6b8325f44380e6bb4833b52df09a8f8fb
LeirbagC <gabriel.chiba@hotmail.com>
2021-08-16 18:35:10 -0400

    Updated layout

--- README.md
+++ README.md
@@ -2,9 +2,12 @@
 
 A Simple Crypto Listing App built with Flutter that show prices and other information of the most popular cryptocurrencies.
 
+
+
 ## Instructions
 
 By default the Api Key is stored in a .env file.
+
 
 It's necessary to create it with the following structure:
 ```dotenv
--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'dart:convert';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'dart:math' as math;
 import 'package:http/http.dart' as http;
+import 'package:cached_network_image/cached_network_image.dart';
 
 Future<List> getCurrencies() async {
   String apiUrl = 'pro-api.coinmarketcap.com';
@@ -33,24 +34,48 @@ Future<List> getCurrencies() async {
   return data;
 }
 
+Future<Map> getCurrencyInfo(int id) async {
+  String apiUrl = 'pro-api.coinmarketcap.com';
+  String idText = id.toString();
+  final queryParameters = {
+    'id': idText
+  };
+
+  final uri =
+  Uri.https(apiUrl, '/v1/cryptocurrency/info', queryParameters);
+  // Print the http request
+  print(uri);
+
+  // Get the key from env and call get method
+  http.Response response = await http.get(uri,
+      headers: {
+        'X-CMC_PRO_API_KEY': dotenv.env['CMC_API'].toString(),
+        'Accept': 'application/json'
+      });
+
+  Map<String, dynamic> map = jsonDecode(response.body)['data'][idText];
+
+  return map;
+}
+
 void main() async{
   // Load api key from .env file in root
   await dotenv.load(fileName: ".env");
   List currencies = await getCurrencies();
   runApp(new MaterialApp(
-    home: new CryptoListWidget(currencies),
+    home: new CryptoList(currencies),
   ));
 }
 
-class CryptoListWidget extends StatelessWidget {
+class CryptoList extends StatelessWidget {
   final List _currencies;
-  CryptoListWidget(this._currencies);
+  CryptoList(this._currencies);
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       body: _buildBody(),
-      backgroundColor: Colors.blue,
+      backgroundColor: Colors.blueAccent,
     );
   }
 
@@ -93,11 +118,11 @@ class CryptoListWidget extends StatelessWidget {
     );
   }
 
-  CircleAvatar _getIconWidget(String currencyName, MaterialColor color) {
-    return new CircleAvatar(
-      backgroundColor: color,
-      child: new Text(currencyName[0],
-        style: TextStyle(color: Colors.white),),
+  CachedNetworkImage _getIconWidget(int currencyId, MaterialColor color) {
+    return new CachedNetworkImage(
+      imageUrl: "https://s2.coinmarketcap.com/static/img/coins/64x64/" + currencyId.toString() +".png",
+      placeholder: (context, url) => CircularProgressIndicator(),
+      errorWidget: (context, url, error) => Icon(Icons.error),
     );
   }
 
@@ -110,25 +135,63 @@ class CryptoListWidget extends StatelessWidget {
 
   Text _getSubtitlePriceWidget(double priceUsd) {
     String price = priceUsd.toStringAsFixed(2);
-    return new Text('R\$ $price');
+    return new Text(
+        'R\$ $price',
+        style: new TextStyle(fontWeight: FontWeight.bold),
+    );
   }
 
-  Text _getSubtitleLastChangeWidget(double percentChange1h) {
+  Row _getSubtitleLastChangeWidget(double percentChange1h) {
     String percent = percentChange1h.toStringAsFixed(2);
-    return new Text('1 hour: $percent%');
+    Icon icon;
+    MaterialColor color;
+    if (percentChange1h >= 0) {
+      color = Colors.green;
+      icon = Icon(
+        Icons.arrow_drop_up,
+        color: Colors.green,
+        size: 30.0,
+      );
+    } else {
+      color = Colors.red;
+      icon = Icon(
+        Icons.arrow_drop_down,
+        color: Colors.red,
+        size: 30.0,
+      );
+    }
+    return new Row (
+      children: [
+        icon,
+        Text(
+            '$percent%',
+            style: TextStyle(color: color)
+        ),
+      ],
+    );
+  }
+
+  Text _getSubtitleSymbol(String slug) {
+    return new Text(slug);
   }
 
   ListTile _getListTile(Map currency, MaterialColor color) {
     return new ListTile(
-      leading: _getIconWidget(currency['name'], color),
-      title: _getTitleWidget(currency['name']),
+      leading: _getIconWidget(currency['id'], color),
+      title: Row (
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          _getTitleWidget(currency['name']),
+          _getSubtitlePriceWidget(currency['quote']['BRL']['price']),
+        ],
+      ),
       subtitle: Padding (
-          padding: const EdgeInsets.only(top: 5.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          padding: EdgeInsets.only(top: 10.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          // crossAxisAlignment: CrossAxisAlignment.end,
           children: [
-            _getSubtitlePriceWidget(currency['quote']['BRL']['price']),
-            SizedBox( height: 2.0),
+            _getSubtitleSymbol(currency['symbol']),
             _getSubtitleLastChangeWidget(currency['quote']['BRL']['percent_change_1h']),
           ],
         ),
@@ -148,4 +211,3 @@ class CryptoListWidget extends StatelessWidget {
     );
   }
 }
-
